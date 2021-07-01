0a11e1a7bd79a3f932dc4f5819cd936e93c49540
Evan Schoenberger <evanjschoenberger@gmail.com>
2021-07-01 12:12:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# YarnSpriteExtractor
+ Run this on a folder containing [Yarn Spinner](https://yarnspinner.dev/) files to recursively
+generate the names of all sprites specified by the command
+`<<setSprite Character spritename>>`
+within those files
+
+```$ python . /Path/To/Your/Game/Assets/Dialogue > out.md```
+
+This is extremely bare bones so use at your own risk
---
+++ __main__.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import re
+files = []
+
+sprites = {}
+
+walk_dir = sys.argv[1]
+walk_dir = os.path.abspath(walk_dir)
+
+files = [os.path.join(root, file)
+         for root, _, files in os.walk(walk_dir) for file in files]
+r = re.compile("^.*\.yarn$")
+files = list(filter(r.match, files))
+
+
+r = re.compile("^<<setSprite (.+) (.+)>>$")
+for file in files:
+    with open(file) as f:
+        for line in f:
+            line = line.strip()
+            match = r.match(line)
+            if match:
+                sprite = match.group(1)
+                expression = match.group(2)
+
+                if sprite not in sprites:
+                    sprites[sprite] = []
+
+                if expression not in sprites[sprite]:
+                    sprites[sprite].append(expression)
+
+
+for key in sorted(sprites):
+    print(f"Sprites needed for {key}:")
+    print()
+    for val in sprites[key]:
+        print(f"- [ ] {val}")
+    print("\n")
